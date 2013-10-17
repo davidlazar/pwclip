971e51e32bd86441fd2e0de1ea13f7c2b725995a
Colin Valliant <alcarithemad@gmail.com>
2013-10-17 04:55:08 -0600

    Gracefully restore clipboard if killed while password is hot

--- pwclip.py
+++ pwclip.py
@@ -60,8 +60,12 @@ def pwclip(pw):
     cb = get_clipboard() 
     set_clipboard(pw)
     print('Password copied to clipboard for 10 seconds.')
-    time.sleep(10)
-    set_clipboard(cb)
+    try:
+        time.sleep(10)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        set_clipboard(cb)
 
 
 def argparser():
